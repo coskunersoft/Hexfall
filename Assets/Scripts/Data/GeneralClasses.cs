99990c0d8b6e2b0dc1f0fbac31b0a4953d17f5a3
Coskunersoft <coskunersoft@outlook.com>
2021-02-02 15:35:36 +0300

    d-1
    First data

---
+++ Assets/Scripts/Data/GeneralClasses.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneralClasses : MonoBehaviour
+{
+    
+}
+
+
+[CreateAssetMenu(fileName = "Settings", menuName = "Hexfall/GameSettings", order = 1)]
+public class GameSettings : ScriptableObject
+{
+    public int Row;
+    public int Column;
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,5 +5,27 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+    public GameSettings gameSettings;
+    public RuntimeVariables runtimeVars;
+
+    public void StartGame()
+    {
+        
+    }
+
+    private void SetupGame()
+    {
+
+    }
+
+    [System.Serializable]
+    public struct RuntimeVariables
+    {
+        public bool IsGameStarted;
+        public GameItem[,] gameGrid;
+       
+    }
+
     
+
 }
